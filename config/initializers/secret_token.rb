15f480278b3162476fe3136f3f770ce81e2ddfc0
Robert Ferney <capnregex@gmail.com>
2014-01-29 15:33:35 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pizzeria::Application.config.secret_key_base = '9a18c5e0bb18089ae7fc59c50c3ee69750a830889dcfcdd7dce06ce4ccebf24257f2ac2f2d60397db6192c626e48d7fb1149a5e5bb5eed9b8ef131184230dc01'
